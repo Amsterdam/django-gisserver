0022dcf2f381a1f728ddca11a5a0f8c203dd65c4
Diederik van der Boor <vdboor@edoburu.nl>
2020-02-13 21:51:03 +0100

    Fix Travis by upgrading libgdal and libproj

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,19 @@
 from pathlib import Path
 
+import django
 import pytest
 from django.contrib.gis.geos import Point
+from django.contrib.gis.gdal import gdal_version
 
 from tests.test_gisserver.models import Restaurant
 
 HERE = Path(__file__).parent
 SRID_RD_NEW = 28992
+
+
+def pytest_configure():
+    gdal_ver = gdal_version().decode()
+    print(f"Running with Django {django.__version__}, GDAL {gdal_ver}")
 
 
 @pytest.fixture()
