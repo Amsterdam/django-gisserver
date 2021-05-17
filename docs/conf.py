684af835d6aeae95e265ef4a3952655a6d25f8eb
Diederik van der Boor <vdboor@edoburu.nl>
2021-05-17 11:41:52 +0200

    docs: make sphinx nitpicky to warn about :py:class:`..` references too

--- docs/conf.py
+++ docs/conf.py
@@ -6,14 +6,15 @@
 
 # -- Path setup --------------------------------------------------------------
 
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
 import os
 import sys
 
 import django
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath("_ext"))
 sys.path.insert(0, os.path.abspath(".."))
 os.environ["DJANGO_SETTINGS_MODULE"] = "djangodummy.settings"
@@ -31,6 +32,8 @@ release = "1.1.2"
 
 
 # -- General configuration ---------------------------------------------------
+
+nitpicky = True
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
