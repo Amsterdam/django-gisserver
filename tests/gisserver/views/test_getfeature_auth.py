beeaa67ed63acbeaa12914dfeecd45c39b09fed5
Pieter Stokkink <p.stokkink@amsterdam.nl>
2025-03-05 20:51:38 +0100

    test auth

--- tests/gisserver/views/test_getfeature_auth.py
+++ tests/gisserver/views/test_getfeature_auth.py
@@ -1,6 +1,6 @@
 import pytest
 
-from tests.constants import NAMESPACES
+from tests.constants import NAMESPACES, XML_NS
 from tests.utils import WFS_20_XSD, read_response, validate_xsd
 
 # enable for all tests in this file
@@ -20,6 +20,29 @@ class TestGetFeature:
         response = client.get(
             "/v1/wfs/?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=denied-feature"
         )
+        content = read_response(response)
+        assert response["content-type"] == "text/xml; charset=utf-8", content
+        assert response.status_code == 403, content
+        assert "</ows:Exception>" in content
+
+        xml_doc = validate_xsd(content, WFS_20_XSD)
+        assert xml_doc.attrib["version"] == "2.0.0"
+        exception = xml_doc.find("ows:Exception", NAMESPACES)
+        assert exception.attrib["exceptionCode"] == "PermissionDenied"
+
+        message = exception.find("ows:ExceptionText", NAMESPACES).text
+        assert message == "No access to this feature."
+
+    def test_post_unauth(self, client):
+        """Prove that features may block access.
+        Note that HTTP 403 is not in the WFS 2.0 spec, but still useful to have.
+        """
+        xml = f"""
+            <GetFeature version="2.0.0" service="WFS" {XML_NS}>
+            <Query typeNames="denied-feature"></Query>
+            </GetFeature>
+            """
+        response = client.post("/v1/wfs/", data=xml, content_type="application/xml")
         content = read_response(response)
         assert response["content-type"] == "text/xml; charset=utf-8", content
         assert response.status_code == 403, content
