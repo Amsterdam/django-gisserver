ee702242a1446a4a9a5ad4a74f101dc1410c726b
Diederik van der Boor <vdboor@edoburu.nl>
2025-04-03 13:09:16 +0200

    cleanup: use BaseNode AST logic for XML parsing of SortBy

--- gisserver/parsers/fes20/sorting.py
+++ gisserver/parsers/fes20/sorting.py
@@ -4,9 +4,10 @@ from dataclasses import dataclass
 from enum import Enum
 
 from gisserver.exceptions import InvalidParameterValue
+from gisserver.parsers.ast import BaseNode, expect_children, expect_tag, tag_registry
 from gisserver.parsers.fes20 import ValueReference
 from gisserver.parsers.query import CompiledQuery
-from gisserver.parsers.xml import NSElement, split_ns, xmlns
+from gisserver.parsers.xml import NSElement, xmlns
 
 FES_SORT_ORDER = xmlns.fes20.qname("SortOrder")
 
@@ -28,18 +29,73 @@ class SortOrder(Enum):
                 "Expect ASC/DESC ordering direction", locator="sortby"
             ) from None
 
+    @classmethod
+    @expect_tag(xmlns.fes20, "SortOrder")
+    def from_xml(cls, element: NSElement):
+        return SortOrder.from_string(element.text)
+
 
 @dataclass
-class SortProperty:
-    """This class name is based on the WFS spec."""
+@tag_registry.register("SortProperty", xmlns.fes20)
+class SortProperty(BaseNode):
+    """This class name is based on the WFS spec.
+
+    This parses and handles the syntax::
+
+        <fes:SortProperty>
+            <fes:ValueReference>name</fes:ValueReference>
+            <fes:SortOrder>ASC</fes:SortOrder>
+        </fes:SortProperty>
+    """
 
     value_reference: ValueReference
     sort_order: SortOrder = SortOrder.ASC
 
+    def __post_init__(self):
+        if "[" in self.value_reference.xpath:
+            raise InvalidParameterValue(
+                "Sorting with XPath attribute selectors is not supported.", locator="sortby"
+            )
+
+    @classmethod
+    @expect_tag(xmlns.fes20, "SortProperty")
+    @expect_children(1, ValueReference, "SortOrder")
+    def from_xml(cls, element: NSElement) -> SortProperty:
+        """Parse the incoming XML"""
+        sort_order = element.find(FES_SORT_ORDER)
+        return cls(
+            value_reference=ValueReference.from_xml(element[0]),
+            sort_order=(
+                SortOrder.from_xml(sort_order) if sort_order is not None else SortOrder.ASC
+            ),
+        )
+
+    @classmethod
+    def from_string(cls, value: str) -> SortProperty:
+        """Parse the incoming GET parameter."""
+        xpath, _, direction = value.partition(" ")
+        return SortProperty(
+            value_reference=ValueReference(xpath),
+            sort_order=SortOrder.from_string(direction) if direction else SortOrder.ASC,
+        )
+
 
 @dataclass
-class SortBy:
-    """The sortBy clause."""
+@tag_registry.register("SortBy", xmlns.fes20)
+class SortBy(BaseNode):
+    """The sortBy clause.
+
+    This parses and handles the syntax::
+
+        <fes:SortBy>
+            <fes:SortProperty>
+                <fes:ValueReference>name</fes:ValueReference>
+                <fes:SortOrder>ASC</fes:SortOrder>
+            </fes:SortProperty>
+        </fes:SortBy>
+
+    It also supports the SORTBY parameter for GET requests.
+    """
 
     sort_properties: list[SortProperty]
 
@@ -51,44 +107,23 @@ class SortBy:
             return cls.from_string(value)
 
     @classmethod
-    def from_string(cls, value: str):
-        """Construct the SortBy object from a KVP "SORTBY" parameter."""
-        props = []
-        for field in value.split(","):
-            if "[" in field:
-                raise InvalidParameterValue(
-                    "sortby", "Sorting with XPath attribute selectors is not supported."
-                )
-
-            if " " in field:
-                xpath, direction = field.split(" ", 1)
-                props.append(
-                    SortProperty(
-                        value_reference=ValueReference(xpath),
-                        sort_order=SortOrder.from_string(direction),
-                    )
-                )
-            else:
-                props.append(SortProperty(value_reference=ValueReference(field)))
-
-        return cls(sort_properties=props)
+    @expect_children(1)
+    def from_xml(cls, element: NSElement) -> SortBy:
+        """Parse the XML tag."""
+        return cls(
+            sort_properties=[
+                # The from_xml() validates that the child node is a <fes:SortProperty>
+                SortProperty.from_xml(child)
+                for child in element
+            ]
+        )
 
     @classmethod
-    def from_xml(cls, element: NSElement):
-        props = []
-        ns, _tag = split_ns(element.tag)
-        for prop in element:
-            sort_order = prop.find(FES_SORT_ORDER)
-            sort_property = SortProperty(
-                value_reference=ValueReference.from_xml(prop[0]),
-                sort_order=(
-                    SortOrder.from_string(sort_order.text)
-                    if sort_order is not None
-                    else SortOrder.ASC
-                ),
-            )
-            props.append(sort_property)
-        return cls(sort_properties=props)
+    def from_string(cls, value: str):
+        """Construct the SortBy object from a KVP "SORTBY" parameter."""
+        return cls(
+            sort_properties=[SortProperty.from_string(field) for field in value.split(",")],
+        )
 
     def build_ordering(self, compiler: CompiledQuery):
         """Build the ordering for the Django ORM call."""
