40440e098b711b4eebb08eaabaa05d98f09468bc
Diederik van der Boor <vdboor@edoburu.nl>
2025-05-12 09:19:14 +0200

    Fix NULL comparisons for incorrectly formatted date/time data
    The parse_date() and parse_time() logic from Django returns None
    when there is a parsing error.

--- tests/gisserver/parsers/fes20/utils.py
+++ tests/gisserver/parsers/fes20/utils.py
@@ -4,16 +4,22 @@ from gisserver.features import FeatureType
 from gisserver.geometries import WGS84
 from gisserver.parsers.fes20 import Filter
 from gisserver.parsers.query import CompiledQuery
-from gisserver.types import ORMPath, XsdElement, XsdTypes
+from gisserver.types import ORMPath, XsdAnyType, XsdElement, XsdTypes
 from tests.test_gisserver import models
 
 
-def compile_query(filter: Filter, feature_type_names: list[str] | None = None) -> CompiledQuery:
+def compile_query(
+    filter: Filter,
+    feature_type_names: list[str] | None = None,
+    field_types: dict[str, XsdAnyType] | None = None,
+) -> CompiledQuery:
     """Allow to compile the filter, without having a whole feature type defined."""
     if feature_type_names is None:
         feature_type_names = ["TestFeature"]
 
-    compiler = CompiledQuery(feature_types=[_MockFeatureType(n) for n in feature_type_names])
+    compiler = CompiledQuery(
+        feature_types=[_MockFeatureType(name, field_types) for name in feature_type_names]
+    )
     q_object = filter.build_query(compiler)
     if q_object is not None:
         compiler.add_lookups(q_object)
@@ -28,15 +34,18 @@ class _MockFeatureType(FeatureType):
     other_crs = []
     model = models.Restaurant
 
-    def __init__(self, name):
+    def __init__(self, name, field_types: dict[str, XsdAnyType] | None):
         self.name = name
         self.xml_name = f"{{{self.xml_namespace}}}{name}"
+        self.field_types = field_types or {}
 
     def resolve_element(self, xpath: str, ns_aliases: dict[str, str]):
         """Allow ValueReference.parse_xpath() to work."""
         parts = [word.strip() for word in xpath.split("/")]
         orm_path = ORMPath(orm_path="__".join(parts), orm_filters=None)
         orm_path.child = XsdElement(
-            name=parts[-1], type=XsdTypes.anyType, namespace=self.xml_namespace
+            name=parts[-1],
+            type=self.field_types.get(xpath, XsdTypes.anyType),
+            namespace=self.xml_namespace,
         )
         return orm_path
