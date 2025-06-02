b4c8672574953bddee82a343cfedab64dbf749bc
Diederik van der Boor <vdboor@edoburu.nl>
2025-06-02 12:41:05 +0200

    Improve styling of the default template

--- example/urls.py
+++ example/urls.py
@@ -8,9 +8,12 @@ from django.urls import include, path
 
 def _root_view(request):
     return HttpResponse(
-        """
-        <title>django-gisserver demo app</title>
-        <style>body { line-height: 1.5; font-family: Arial, sans-serif; }</style>
+        """<!doctype html>
+        <html><head>
+          <title>django-gisserver demo app</title>
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+          <style> body { margin: 2rem; } </style>
+        </head>
         <body>
           <h1>Demo of django-gisserver</h1>
           <ul>
@@ -18,6 +21,7 @@ def _root_view(request):
             <li><a href="/wfs/">WFS Server</a></li>
           </ul>
         </body>
+        </html>
         """
     )
 
